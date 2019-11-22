4b603eb953a143da974e3a814c45f677a4aa8044
giventocode <3589801+giventocode@users.noreply.github.com>
2019-11-21 23:58:16 -0500

    requirements are read

--- setup.py
+++ setup.py
@@ -5,10 +5,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 version = nuttercli.get_cli_version()
+
+def parse_requirements(filename):
+    """Load requirements from a pip requirements file."""
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
 setuptools.setup(
     name="nutter",
     version=version,
     author="Jesus Aguilar, Rob Bagby",
+    install_requires=parse_requirements("requirements.txt"),
     author_email="jesus.aguilar@microsoft.com, rob.bagby@microsoft.com",
     description="A databricks notebook testing library",
     long_description="A databricks notebook testing library",
