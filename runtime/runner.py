1f9ac3ca10699efc46e0a32e6569524c57062caf
Thomas Conté <tconte@microsoft.com>
2021-06-24 11:24:20 +0200

    Parallel runner

---
+++ runtime/runner.py
@@ -0,0 +1,50 @@
+"""
+Copyright (c) Microsoft Corporation.
+
+Licensed under the MIT license.
+"""
+
+import common.scheduler as scheduler
+from common.testexecresults import TestExecResults
+from common.testresult import TestResults
+
+from runtime.nutterfixture import NutterFixture
+
+
+class NutterRunner(object):
+    """Helper class to execute tests in parallel."""
+
+    def __init__(self, tests, num_of_workers=1):
+        """Initialize the runner.
+
+        Args:
+            tests (list): the list of tests to execute.
+            num_of_workers (int): number of parallel workers.
+        """
+        for i in tests:
+            if not isinstance(i, NutterFixture):
+                raise TypeError("tests elements must be of type NutterFixture")
+        self.tests = tests
+        self.num_of_workers = num_of_workers
+
+    def execute_tests(self):
+        """Execute the tests."""
+        sched = scheduler.get_scheduler(self.num_of_workers)
+
+        for i in self.tests:
+            sched.add_function(i.execute_tests)
+
+        results = sched.run_and_wait()
+
+        return self._collect_results(results)
+
+    def _collect_results(self, results):
+        """Collect all results in a single TestExecResults object."""
+        all_results = TestResults()
+
+        for funcres in results:
+            if funcres.func_result is not None:
+                for testres in funcres.func_result.test_results.results:
+                    all_results.append(funcres.func_result.test_results.results)
+
+        return TestExecResults(all_results)
---
+++ tests/runtime/test_runner.py
@@ -0,0 +1,134 @@
+"""
+Copyright (c) Microsoft Corporation.
+
+Licensed under the MIT license.
+"""
+
+import time
+
+import pytest
+
+from runtime.nutterfixture import NutterFixture
+from runtime.runner import NutterRunner
+
+test_cases = [
+    (1, 1),
+    (1, 2),
+    (2, 1),
+    (2, 2),
+    (3, 1),
+    (3, 2),
+    (3, 3)
+]
+
+@pytest.mark.parametrize('num_of_tests, num_of_workers', test_cases)
+def test__execute_tests__x_tests_x_workers__results_ok(num_of_tests, num_of_workers):
+    # Assemble list of tests
+    tests = []
+    for i in range(num_of_tests):
+        test_case = RunnerTestFixture()
+        tests.append(test_case)
+
+    # Execute tests
+    runner = NutterRunner(tests, num_of_workers)
+    results = runner.execute_tests()
+
+    # Assert results
+    assert len(results.test_results.results) == len(tests)
+    assert results.test_results.passed() == True
+
+def test__execute_tests__3_tests_in_sequence_with_failed_assertion__results_ok():
+    # Arrange
+    tests = [
+        RunnerTestFixture(),
+        RunnerTestFixtureFailAssert(),
+        RunnerTestFixture()
+    ]
+
+    # Act
+    runner = NutterRunner(tests, 1)
+    results = runner.execute_tests()
+
+    # Assert
+    assert len(results.test_results.results) == len(tests)
+    assert results.test_results.results[0].passed == True
+    assert results.test_results.results[1].passed == False
+    assert results.test_results.results[2].passed == True
+
+def test__execute_tests__3_tests_in_sequence_with_run_exception__results_ok():
+    # Arrange
+    tests = [
+        RunnerTestFixture(),
+        RunnerTestFixtureRunException(),
+        RunnerTestFixture()
+    ]
+
+    # Act
+    runner = NutterRunner(tests, 1)
+    results = runner.execute_tests()
+
+    # Assert
+    assert len(results.test_results.results) == len(tests)
+    assert results.test_results.results[0].passed == True
+    assert results.test_results.results[1].passed == False
+    assert results.test_results.results[2].passed == True
+
+def test__execute_tests__3_tests_in_sequence_with_exec_exception__results_ok():
+    # Arrange
+    tests = [
+        RunnerTestFixture(),
+        RunnerTestFixtureExecuteException(),
+        RunnerTestFixture()
+    ]
+
+    # Act
+    runner = NutterRunner(tests, 1)
+    results = runner.execute_tests()
+
+    # Assert
+    assert len(results.test_results.results) == len(tests) - 1
+    assert results.test_results.results[0].passed == True
+    assert results.test_results.results[1].passed == True
+
+class RunnerTestFixture(NutterFixture):
+    def before_test(self):
+        pass
+
+    def run_test(self):
+        pass
+
+    def assertion_test(self):
+        assert 1 == 1
+
+    def after_test(self):
+        pass
+
+class RunnerTestFixtureFailAssert(NutterFixture):
+    def before_test(self):
+        pass
+
+    def run_test(self):
+        pass
+
+    def assertion_test(self):
+        assert 1 != 1
+
+    def after_test(self):
+        pass
+
+class RunnerTestFixtureRunException(NutterFixture):
+    def before_test(self):
+        pass
+
+    def run_test(self):
+        raise(Exception())
+
+    def assertion_test(self):
+        assert 1 == 1
+
+    def after_test(self):
+        pass
+
+class RunnerTestFixtureExecuteException(NutterFixture):
+    def execute_tests(self):
+        raise(Exception())
